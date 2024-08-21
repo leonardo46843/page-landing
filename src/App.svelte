b631548deee0b8ce20cd9956eb77a669794f0500
Leonardo~ <140676545+leonardo46843@users.noreply.github.com>
2024-08-21 13:06:24 +0700

    nav prep

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<main>
+  <header>
+    <a href="#hero">WPU</a>
+    <nav>
+      <ul>
+        <li><a href="#hero">Beranda</a></li>
+        <li><a href="#about">Tentang Kami</a></li>
+        <li><a href="#profile">Pengelola</a></li>
+        <li><a href="#contact">Kontak</a></li>
+      </ul>
+    </nav>
+  </header>
+</main>
